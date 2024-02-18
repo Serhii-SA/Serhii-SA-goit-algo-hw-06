3c7cd8dac4c6c2a006b793a54d1f35d52a4665dc
Serhii-SA <serg.a.tolstov@gmail.com>
2024-02-18 23:40:52 +0200

    sec com

--- hw_6_1.py
+++ hw_6_1.py
@@ -1,3 +1,5 @@
+# Прошу вибачення ,не встиг додати нормальні коменти до коду ((
+
 from collections import UserDict
 
 class Field:
@@ -8,7 +10,7 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-		pass
+        pass
 
 class Phone(Field):
 		pass
@@ -17,13 +19,25 @@ class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
-        
+
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
-    def add_phone(self,ph):
-        if len(ph)== 10:
-            self.phones.append(Phone(ph))
+    def add_phone(self,ph_num_to_add):
+        if len(ph_num_to_add)== 10:
+            self.phones.append(Phone(ph_num_to_add))
+        else:
+            print("Phonenumber has invalid format")
+
+    def remove_phone(self,ph_num_to_del):
+        if len(ph_num_to_del)== 10:
+            pos=0
+            for n_del in self.phones:
+                if ph_num_to_del == n_del.value:
+                    self.phones.pop(pos)
+                else:
+                    pos +=1
+            print(f"No < {ph_num_to_del} > in this contact")
         else:
             print("Phonenumber has invalid format")
 
@@ -51,14 +65,21 @@ class AddressBook(UserDict):
                     if name.value == nm :
                         return record
                 print(f"No < {nm} > contact in the phonebook")
-              
+        def delete(self,nm):
+            for name, record in self.data.items():                
+                if name.value == nm :
+                    n_del = name
+            try:         
+                self.data.pop(n_del)
+            except Exception:
+                print(f"No < {nm} > contact in the phonebook") 
          
 book=AddressBook()
 
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555") 
-book.add_record(john_record)
+jul_record = Record("John")
+jul_record.add_phone("1234567890")
+jul_record.add_phone("5555555555") 
+book.add_record(jul_record)
 
 
 jane_record = Record("Jane")
@@ -78,3 +99,19 @@ book.show_all()
 
 found_phone = john.find_phone("5555555555")
 print(f"{john.name}: {found_phone}")  
+
+book.delete("Jane")
+
+book.show_all()
+
+jul_record = Record("Jul")
+jul_record.add_phone("1234567890")
+jul_record.add_phone("5555555000") 
+book.add_record(jul_record)
+
+book.show_all()
+
+jul_record.remove_phone("1234567891") # спроба видалити неіснуючий номер
+jul_record.remove_phone("1234567890")
+book.add_record(jul_record)
+book.show_all()
